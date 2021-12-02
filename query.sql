608382d13b4a87d78e37e9b29c652b7ff68579f2
louisrubin <miguel10.rbn@gmail.com>
2021-12-02 13:05:35 -0300

    first commit

---
+++ query.sql
@@ -0,0 +1,25 @@
+-- insert into inventario (nombre, stock)
+-- values ('chomba',10),
+-- 		('buzo',5),
+-- 		('pollera',8)
+
+
+-- insert into proveedores(nombre, domicilio, telefono)
+-- values ('FP','Av. Sarmiento 1505',3624968574)
+-- 
+-- insert into cliente (dni, nombre, apellido, domicilio, telefono)
+-- VALUES (43258102,'Germán','Peralta','Av. 9 de Julio 3220',3625100285),
+-- 	(44123654,'Axel','Fernandez','J. Milei 360',3624202858),
+-- 		(43982753,'Monica','Castambide','R. Starkey 2800',3624101080),
+-- 		(44123123,'Kiara','Lopéz','Laprida 220',3624306399),
+-- 	(44852103,'Gustavo','Kupinski','Lennon 1765',3624306399),
+-- 		(42741123,'Linda','McCartney','G. Cerati 2014',3625743050),
+-- 		(45200633,'Jorge','Harrison','Something 1969',3625632251)
+-- 		
+-- update cliente (domilicio)
+-- value ('Gustavo Kupinski ')
+
+-- 
+-- delete from inventario where id = 4
+
+
