ab00fe95acbb66baffa27b0e83a4b5e08e792422
louisrubin <miguel10.rbn@gmail.com>
2021-12-04 15:08:45 -0300

    programa terminado

--- db.py
+++ db.py
@@ -1,35 +1,28 @@
 import sqlite3
-from sqlite3.dbapi2 import Cursor
 import time
 import os
-
 
 
 """
 ==============================================================
 
+CONSIGNA: 
 
 La escuela tiene remeras, buzos, polleras y chombas para vender (que fabrica la gente de FP). 
 Hacer un sistema que permita llevar el stock (cuando entran, quien las llevan y que cantidades)
 y cuando se venden, con nro de factura, etc.
 
-
-
 ==============================================================
 """
-
-
-
 
 def insertar_datos(tabla_num):
     conexion = conectar_base_datos()
     cursor = conexion.cursor()
-    query = generar_query_insert(tabla_id= tabla_num)
+    query1 = generar_query_insert(tabla_id= tabla_num)
 
     compra = False
+
     datos = []
-    stock = cursor.execute("select stock from inventario where id = 2").fetchall()
-    print(stock)
 
     for column in TABLAS[tabla_num-1]['columns']:
 
@@ -44,51 +37,92 @@ def insertar_datos(tabla_num):
             datos.append(t2)
 
         else:
-            data = input(f" INGRESE {column.upper()}: ")
-            """
+            data = input(f" (X - SALIR) - INGRESE {column.upper()}: ")
+
+            if data.lower() != 'x':
+                
                 if column == 'cantidad':
-                    cantidad = data
+
+                    stock = list(sub[0] for sub in cursor.execute("SELECT stock FROM inventario WHERE nombre = '" + datos[0] + "'").fetchall())[0]      # res = list(sub[0] for sub in test_tuple)
+                    id_update = list(sub[0] for sub in cursor.execute("SELECT id FROM inventario WHERE nombre = '" + datos[0] + "'").fetchall())[0]
+
+                    
+                    new_cantidad = stock - int(data)
                     compra = True
-            """
-            datos.append(data)
+                
+                datos.append(data)  
+            else:
+                break
 
-    #if compra:
+    if compra == True:      # SI LA VARIABLE 'compra' ES VERDADERO SIGNIFICA UNA COMPRA Y GENERA UNA QUERY  'UPDATE' RESTANTO EL STOCK
+        query2 = "UPDATE inventario SET stock = " + str(new_cantidad) + " WHERE id = " + str(id_update)
+        cursor.execute(query2)
     
-            
-    print("DATOS AGREGADOS: ",datos)
-    input("\n<continue>")
+    try:
+        cursor.execute(query1,datos)
+        conexion.commit()
+        print("--------------------------------------------------------------------------------------------")
+        print("\nDATOS AGREGADOS: ",datos)
+        print("\n... STOCK DEL INVENTARIO ACTUALIZADO ...")
+        input("\n<continue>")
+    except:
+        print("-------------------------------------------")
+        print("\n... LOS DATOS NO FUERON AGREGADOS ...")
+        input("\n<continue>")
 
-    cursor.execute(query,datos)
-    conexion.commit()
 
 
 def consultar_compra(wea=""):
     if wea is None:
-        nro_factura = input("INGRESE EL NUMERO DE FACTURA: ")
-        query = "SELECT * FROM compra WHERE nro_factura = "+ nro_factura
-
         conexion = sqlite3.connect('base_de_datos.db')
-        con = conexion.execute(query)
-        resul = con.fetchall()
+        cursor = conexion.cursor()
 
-        if len(resul) == 0:
+        nro_factura = input("INGRESE EL NUMERO DE FACTURA: ")                       # res = list(sub[0] for sub in test_tuple)
+        query1 = "SELECT * FROM compra WHERE nro_factura = "+ nro_factura
+
+        try:
+            dni = list(sub[5] for sub in cursor.execute("SELECT * FROM compra WHERE nro_factura = " + nro_factura ).fetchall())[0]
+            query2 = "SELECT * FROM cliente WHERE dni = " + str(dni)
+
+            print(" \n DATOS DE LA COMPRA: ",conexion.execute(query1).fetchall())
+            print(" DATOS DEL CLIENTE: ",conexion.execute(query2).fetchall())
+            input("\n<continue>")
+
+        except:
             print(" ..... NO HAY REGISTRO DE COMPRAS .....\n")
             input("\n<continue>")
-        else:
-            print(resul)
-            input("\n<continue>")
+
+
 
 def ver_stock(wea=""):
-    pass
+    if wea is None:
+        conexion = conectar_base_datos()
+        cursor = conexion.cursor()
+
+        stock = cursor.execute("SELECT nombre, stock FROM inventario").fetchall()
+
+        print("\n------------------ INVENTARIO ------------------")
+        for x in stock:
+            print(x)
+        print("----------------------------------------------")
+        input("\n<continue>")
+
+
+
 
 def ver_proveedores(wea=""):
-    pass
+    if wea is None:
+        conexion = conectar_base_datos()
+        cursor = conexion.cursor()
+        print("\n----------------------- PROVEEDORES -----------------------\n")
+        print(cursor.execute("SELECT nombre, domicilio, telefono FROM proveedores").fetchall())
+        print("--------------------------------------------------------")
+        input("\n<continue>")
 
 
 MENU = [
     {'id': 1, 'nombre':'VENTAS','submenu':[{'id':1, 'nombre':'AGREGAR COMPRA', 'funcion': insertar_datos, 'tab_id': 2},   # agregar compra, consultar alguna venta, 
-                                           {'id':2, 'nombre':'CONSULTAR VENTA', 'funcion': consultar_compra, 'tab_id': None},
-                                           {'id':3, 'nombre':'ELIMINAR VENTA', 'funcion': ver_stock, 'tab_id': None}
+                                           {'id':2, 'nombre':'CONSULTAR VENTA', 'funcion': consultar_compra, 'tab_id': None}
                                             ]},                                     
 
     {'id': 2, 'nombre':'STOCK','submenu':[{'id':1, 'nombre':'VER STOCK', 'funcion': ver_stock, 'tab_id': None},     # consultar stock de algun producto, agregar stock
@@ -130,7 +164,7 @@ def imprimir_menu(menu=MENU):
     if opcion1 == 4:
         quit()          # AQUI SI EL USUARIO INGRESA EL NUMERO 4 TERMINA EL PROGRAMA
 
-    else:
+    elif opcion1 < n:
     
         n = 1
         for subitem in menu[opcion1-1]['submenu']:       # este segundo FOR imprime el 'sub-menu' de cada item
@@ -144,7 +178,8 @@ def imprimir_menu(menu=MENU):
         tab_id = menu[opcion1-1]['submenu'][opcion2-1]['tab_id']
 
         return funcion(tab_id)
-
+    else:
+        pass
 
 
 def conectar_base_datos():
@@ -157,18 +192,14 @@ def creacion_tablas():
     cursor = conectar_base_datos()
 
     tabla_clientes = "CREATE TABLE IF NOT EXISTS cliente(dni integer PRIMARY KEY NOT NULL, nombre text NOT NULL, apellido text NOT NULL, domicilio text NOT NULL, telefono integer NOT NULL)"
-    tabla_compra = "CREATE TABLE IF NOT EXISTS compra(nro_factura integer PRIMARY KEY NOT NULL, producto text NOT NULL, cantidad int NOT NULL, importe integer NOT NULL, forma_pago text NOT NULL, dni_cliente integer NOT NULL, fecha date NOT NULL, hora time NOT NULL, FOREIGN KEY(dni_cliente) REFERENCES cliente(dni))"
+    tabla_compra = "CREATE TABLE IF NOT EXISTS compra(nro_factura integer PRIMARY KEY NOT NULL, producto text NOT NULL, cantidad int NOT NULL, importe integer NOT NULL, forma_pago text NOT NULL, dni_cliente integer NOT NULL, fecha date NOT NULL, hora time NOT NULL)"
     tabla_ropas = "CREATE TABLE IF NOT EXISTS inventario(ID integer PRIMARY KEY NOT NULL, nombre text NOT NULL, stock integer NOT NULL)"
     tabla_proveedores = "CREATE TABLE IF NOT EXISTS proveedores(ID integer PRIMARY KEY NOT NULL, nombre text NOT NULL, domicilio text NOT NULL, telefono integer NOT NULL)"
-    tabla_compra_ropas = "CREATE TABLE IF NOT EXISTS compra_ropas(nro_factura_compra integer NOT NULL, ID_ropas integer NOT NULL, FOREIGN KEY(nro_factura_compra) REFERENCES compra(nro_factura), FOREIGN KEY(ID_ropas) REFERENCES ropas(ID))"
-    tabla_prove_ropas = "CREATE TABLE IF NOT EXISTS prov_ropas(ID_prov integer NOT NULL, ID_ropas integer NOT NULL, hora time PRYMARY KEY NOT NULL, fecha date NOT NULL, FOREIGN KEY(ID_prov) REFERENCES proveedores(ID), FOREIGN KEY(ID_ropas) REFERENCES ropas(ID))"
 
     cursor.execute(tabla_clientes)
     cursor.execute(tabla_compra)
     cursor.execute(tabla_ropas)
     cursor.execute(tabla_proveedores)
-    cursor.execute(tabla_compra_ropas)
-    cursor.execute(tabla_prove_ropas)
 
 def generar_query_insert(tabla_id, lista=TABLAS):
     # funcion para generar una INSERT consulta automática
@@ -193,35 +224,22 @@ def generar_query_insert(tabla_id, lista=TABLAS):
         cont += 1
 
     sql = sql1 + sql2
-    print(sql)
 
     return sql     # RETORNA LA QUERY
 
 
 
-
-
 # ====================================================================================================================
 # ====================================================================================================================
 # ====================================================================================================================
 # ====================================================================================================================
 
 
-#conectar_base_datos()
 creacion_tablas()
 
-#generar_query_insert(2)
 while True:
     os.system("cls")
     imprimir_menu()
-
-"""
-sql = "select nombre, stock from inventario"
-conexion = sqlite3.connect('base_de_datos.db')
-
-con = conexion.execute(sql)
-resul = con.fetchall()
-print(resul)  """
 
 
 
--- query.sql
+++ query.sql
@@ -9,17 +9,12 @@
 -- 
 -- insert into cliente (dni, nombre, apellido, domicilio, telefono)
 -- VALUES (43258102,'Germán','Peralta','Av. 9 de Julio 3220',3625100285),
--- 	(44123654,'Axel','Fernandez','J. Milei 360',3624202858),
+-- 		(44332040, 'Paulo', 'Londra', 'Av. Alcorta 1990',3624824628),
+-- 		(44123654,'Axel','Fernandez','J. Milei 360',3624202858),
 -- 		(43982753,'Monica','Castambide','R. Starkey 2800',3624101080),
 -- 		(44123123,'Kiara','Lopéz','Laprida 220',3624306399),
 -- 	(44852103,'Gustavo','Kupinski','Lennon 1765',3624306399),
 -- 		(42741123,'Linda','McCartney','G. Cerati 2014',3625743050),
 -- 		(45200633,'Jorge','Harrison','Something 1969',3625632251)
--- 		
--- update cliente (domilicio)
--- value ('Gustavo Kupinski ')
-
--- 
--- delete from inventario where id = 4
 
 
