085de717468cd40ae95235d26908b9a1821cc1e1
louisrubin <miguel10.rbn@gmail.com>
2021-12-07 21:49:43 -0300

    agregue comentarios al codigo

--- query.sql
+++ query.sql
@@ -1,20 +1,20 @@
--- insert into inventario (nombre, stock)
--- values ('chomba',10),
--- 		('buzo',5),
--- 		('pollera',8)
-
-
--- insert into proveedores(nombre, domicilio, telefono)
--- values ('FP','Av. Sarmiento 1505',3624968574)
--- 
--- insert into cliente (dni, nombre, apellido, domicilio, telefono)
--- VALUES (43258102,'Germán','Peralta','Av. 9 de Julio 3220',3625100285),
--- 		(44332040, 'Paulo', 'Londra', 'Av. Alcorta 1990',3624824628),
--- 		(44123654,'Axel','Fernandez','J. Milei 360',3624202858),
--- 		(43982753,'Monica','Castambide','R. Starkey 2800',3624101080),
--- 		(44123123,'Kiara','Lopéz','Laprida 220',3624306399),
+-- INSERT INTO cliente(dni, nombre, apellido,domicilio, telefono)
+-- VALUES (44123654,'Axel','Fernandez','J. Milei 360',3624202858),
+-- 		(43258102,'Germán','Peralta','Av. 9 de Julio 3220',3625100285),
 -- 	(44852103,'Gustavo','Kupinski','Lennon 1765',3624306399),
--- 		(42741123,'Linda','McCartney','G. Cerati 2014',3625743050),
--- 		(45200633,'Jorge','Harrison','Something 1969',3625632251)
+-- 		(45200633,'Jorge','Harrison','Something 1969',3625632251),
+-- 		(44123123,'Kiara','Lopéz','Laprida 220',3624306399),
+-- (42741123,'Linda','McCartney','G. Cerati 2014',3625743050),
+-- 		(43982753,'Monica','Castambide','R. Starkey 2800',3624101080),
+-- 		(44332040,'Paulo','Londra','Av. Alcorta 1990',3624824628)
+
+-- INSERT INTO inventario(nombre, stock)
+-- VALUES ('chomba',	10),
+-- 		('buzo',	5),
+-- 		('pollera',	15)
 
 
+
+-- delete from compra
+-- delete from inventario
+-- delete from cliente
